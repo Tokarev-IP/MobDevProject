ddea170af78878b65c66c823191ffa6ecd8b5d68
TokarevIP <tokarevrab@gmail.com>
2024-05-04 00:53:34 +0100

    resolve #6

--- app/src/main/java/com/example/mobdevproject/content/presentation/ContentActivityCompose.kt
+++ app/src/main/java/com/example/mobdevproject/content/presentation/ContentActivityCompose.kt
@@ -1,51 +1,81 @@
 package com.example.mobdevproject.content.presentation
 
+import android.content.Context
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.mobdevproject.content.presentation.screens.mymenu.CheckMyMenuScreen
-import com.example.mobdevproject.content.presentation.screens.mymenu.EditMyMenuMainData
+import com.example.mobdevproject.content.presentation.screens.mymenu.EditScreen
+import com.example.mobdevproject.content.presentation.screens.search.SearchScreen
+import com.example.mobdevproject.content.presentation.screens.settings.SettingsScreen
 
 @Composable
 fun ContentActivityCompose(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
-    startDestination: String = NavHostRoute.CheckMyMenuScreen.route,
-    contentViewModel: ContentViewModel = hiltViewModel()
+    startDestination: String = ContentActivityNavHostRoute.SearchScreen.route,
+    context: Context,
+    onSignOut: () -> Unit,
 ) {
-    val uiState by contentViewModel.getUiStateFlow().collectAsState()
-    val screenStates by contentViewModel.getScreenStateFlow().collectAsState()
-
-    when (screenStates) {
-        is ScreenStates.CheckMyMenuScreen -> {
-            navController.navigate(NavHostRoute.CheckMyMenuScreen.route)
-        }
-        is ScreenStates.CreateMyMenuScreen -> {
-            navController.navigate(NavHostRoute.CreateMyMenuScreen.route)
-        }
-        is ScreenStates.EditMyMenuMainData -> {
-            navController.navigate(NavHostRoute.EditMyMenuMainData.route)
-        }
-        is ScreenStates.MenuListScreen -> {
-            navController.navigate(NavHostRoute.MenuListScreen.route)
-        }
-        is ScreenStates.MenuMapScreen -> {
-            navController.navigate(NavHostRoute.MenuMapScreen.route)
-        }
-    }
-
     Scaffold(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize(),
+        bottomBar = {
+            NavigationBar {
+                var selectedItem by rememberSaveable { mutableIntStateOf(1) }
+
+                NavigationBarItem(
+                    icon = { Icon(Icons.Filled.Search, contentDescription = "Search") },
+                    label = { Text(text = "Search") },
+                    selected = selectedItem == 1,
+                    onClick = {
+                        if (selectedItem != 1) {
+                            selectedItem = 1
+                            navController.navigate(route = ContentActivityNavHostRoute.SearchScreen.route)
+                        }
+                    }
+                )
+                NavigationBarItem(
+                    icon = { Icon(Icons.Filled.Edit, contentDescription = "Edit") },
+                    label = { Text(text = "Edit") },
+                    selected = selectedItem == 2,
+                    onClick = {
+                        if (selectedItem != 2) {
+                            selectedItem = 2
+                            navController.navigate(route = ContentActivityNavHostRoute.EditScreen.route)
+                        }
+                    }
+                )
+                NavigationBarItem(
+                    icon = { Icon(Icons.Filled.Settings, contentDescription = "Settings") },
+                    label = { Text(text = "Settings") },
+                    selected = selectedItem == 3,
+                    onClick = {
+                        if (selectedItem != 3) {
+                            selectedItem = 3
+                            navController.navigate(route = ContentActivityNavHostRoute.SettingsScreen.route)
+                        }
+                    }
+                )
+            }
+        },
     ) { padding: PaddingValues ->
 
         NavHost(
@@ -55,28 +85,20 @@ fun ContentActivityCompose(
             navController = navController,
             startDestination = startDestination,
         ) {
-            composable(route = NavHostRoute.CheckMyMenuScreen.route) {
-                CheckMyMenuScreen(
-                    uiState = uiState,
-                    onCreateMenu = { /*TODO*/ },
-                    onRefresh = {/*TODO*/ }
+            composable(route = ContentActivityNavHostRoute.SearchScreen.route) {
+                SearchScreen(context = context)
+            }
+
+            composable(route = ContentActivityNavHostRoute.EditScreen.route) {
+                EditScreen(context = context)
+            }
+
+            composable(route = ContentActivityNavHostRoute.SettingsScreen.route) {
+                SettingsScreen(
+                    onSignOut = { onSignOut() }
                 )
             }
-            composable(route = NavHostRoute.CreateMyMenuScreen.route) {
-//                CreateMyMenuScreen(
-//                    menuMainData = ,
-//                    menuDataList =
-//                )
-            }
-            composable(route = NavHostRoute.EditMyMenuMainData.route) {
-                EditMyMenuMainData(
-                    uiState = uiState,
-                    onChoosePicture = { /*TODO*/ },
-                    onClearPicture = { /*TODO*/ },
-                    onSave = { /*TODO*/ },
-                    onCancel = { /*TODO*/ }
-                )
-            }
+
         }
     }
 }
--- app/src/main/java/com/example/mobdevproject/content/presentation/States.kt
+++ app/src/main/java/com/example/mobdevproject/content/presentation/States.kt
@@ -1,28 +1,121 @@
 package com.example.mobdevproject.content.presentation
 
-interface UiStates{
-    object Show: UiStates
-    object Loading: UiStates
-    object Error: UiStates
+import com.example.mobdevproject.content.data.MenuData
+import com.example.mobdevproject.content.data.MenuMainData
+import com.example.mobdevproject.content.data.MyMenuId
+
+interface UiStates {
+    object Show : UiStates
+    object Loading : UiStates
+    object Error : UiStates
 }
 
-sealed class NavHostRoute(val route: String) {
-    data object CheckMyMenuScreen: NavHostRoute("CheckMyMenuScreen")
-    data object CreateMyMenuScreen: NavHostRoute("CreateMyMenuScreen")
-    data object EditMyMenuMainData: NavHostRoute("EditMyMenuMainData")
-    data object MenuListScreen: NavHostRoute("MenuListScreen")
-    data object MenuMapScreen: NavHostRoute("MenuMapScreen")
+sealed class ContentActivityNavHostRoute(val route: String) {
+    data object EditScreen : EditNavHostRoute("editScreen")
+    data object SearchScreen : EditNavHostRoute("searchScreen")
+    data object SettingsScreen : EditNavHostRoute("settingsScreen")
 }
 
-sealed interface UiIntents{
-    object CreateMenu: UiIntents
-    object  UploadMainMenuData: UiIntents
+sealed class EditNavHostRoute(val route: String) {
+    data object CheckMyMenuScreen : EditNavHostRoute("checkMyMenuScreen")
+    data object EditMyMenuMainScreen : EditNavHostRoute("editMyMenuMainScreen")
+    data object EditMyMenuItemScreen : EditNavHostRoute("editMyMenuItemScreen")
+    data object EditMyMenuScreen : EditNavHostRoute("editMyMenuScreen")
+    data object MenuListScreen : EditNavHostRoute("menuListScreen")
+    data object StartDestinationCompose : EditNavHostRoute("startDestinationCompose")
+    data object QrCodeScreen : EditNavHostRoute("qrCodeScreen")
 }
 
-interface ScreenStates{
-    object CheckMyMenuScreen: ScreenStates
-    object CreateMyMenuScreen: ScreenStates
-    object EditMyMenuMainData: ScreenStates
-    object MenuListScreen: ScreenStates
-    object MenuMapScreen: ScreenStates
+sealed class SearchNavHostRoute(val route: String) {
+    data object MenuListScreen : EditNavHostRoute("menuListScreen")
+    data object MenuScreen : EditNavHostRoute("menuScreen")
+    data object StartDestinationCompose : EditNavHostRoute("startDestinationCompose")
 }
+
+sealed interface MyMenuUiIntents {
+    data class UploadMenuData(val menuData: MenuData, val byteArray: ByteArray?) : MyMenuUiIntents {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as UploadMenuData
+
+            if (menuData != other.menuData) return false
+            if (byteArray != null) {
+                if (other.byteArray == null) return false
+                if (!byteArray.contentEquals(other.byteArray)) return false
+            } else if (other.byteArray != null) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = menuData.hashCode()
+            result = 31 * result + (byteArray?.contentHashCode() ?: 0)
+            return result
+        }
+    }
+
+    data class UploadMainMenuData(val menuMainData: MenuMainData, val byteArray: ByteArray?) :
+        MyMenuUiIntents {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as UploadMainMenuData
+
+            if (menuMainData != other.menuMainData) return false
+            if (byteArray != null) {
+                if (other.byteArray == null) return false
+                if (!byteArray.contentEquals(other.byteArray)) return false
+            } else if (other.byteArray != null) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = menuMainData.hashCode()
+            result = 31 * result + (byteArray?.contentHashCode() ?: 0)
+            return result
+        }
+    }
+
+    data class CreateMyMenu(val myMenuId: MyMenuId) : MyMenuUiIntents
+    data object CheckMyMenu : MyMenuUiIntents
+    data object DeletePicture : MyMenuUiIntents
+    data class GoToEditMyMenuMainScreen(val menuMainData: MenuMainData) : MyMenuUiIntents
+    data class GoToEditMyMenuItemScreen(val menuData: MenuData) : MyMenuUiIntents
+    data class GoToQrCodeScreen(val menuId: String) : MyMenuUiIntents
+    data object NullStateScreen: MyMenuUiIntents
+}
+
+sealed interface SearchUiIntents {
+    data class UseQrCode(val qrCodeText: String?) : SearchUiIntents
+    data object GetAllMenus : SearchUiIntents
+    data class GoToMenuScreenUsingQr(val text: String) : SearchUiIntents
+    data class GoToMenuScreen(val menuMainData: MenuMainData) : SearchUiIntents
+    data object NullScreenState : SearchUiIntents
+}
+
+interface EditScreenStates {
+    object CheckMyMenuScreen : EditScreenStates
+    data class EditMyMenuMainScreen(val menuMainData: MenuMainData) : EditScreenStates
+    object MenuListScreen : EditScreenStates
+    object EditMyMenuScreen : EditScreenStates
+    data class EditMyMenuItemScreen(val menuData: MenuData) : EditScreenStates
+    data class QrCodeScreen(val menuId: String) : EditScreenStates
+    object GoBackNavigation : EditScreenStates
+}
+
+interface SearchScreenStates {
+    object MenuListScreen : SearchScreenStates
+    object MenuScreen : SearchScreenStates
+}
+
+interface SettingsUiIntents {
+    data object LoadData: SettingsUiIntents
+    data class UpdateEmail(val email: String) : SettingsUiIntents
+    data object SignOut: SettingsUiIntents
+    data object VerifyEmail: SettingsUiIntents
+}
+
